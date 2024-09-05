fb632575e8ccd93394356b27b3155ad9e2debe16
Xo1otl <xolotl.mictl4n@gmail.com>
2024-09-05 10:50:43 +0000

    chore: Update git-lfs configuration in postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,7 +6,8 @@
   (
     git lfs install --skip-repo &&
     git -C /workspaces/mictlan remote set-url origin git@mictlan:Xo1otl/mictlan.git && 
-    git -C /workspaces/mictlan lfs pull
+    git -C /workspaces/mictlan lfs pull &&
+    git -C /workspaces/mictlan config pull.rebase true
   ) &
   wait
 )
