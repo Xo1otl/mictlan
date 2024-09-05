d5809b78e674b8f37a206a199de29a4096b06231
Xo1otl <xolotl.mictl4n@gmail.com>
2024-09-05 05:57:37 +0000

    設定ミス直した

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,7 +4,7 @@
   (/home/vscode/.bun/bin/bun install --cwd /workspaces/mictlan/js) &
   (/home/vscode/.local/bin/poetry install --directory /workspaces/mictlan/python --no-root) &
   (
-    git lfs install &&
+    git lfs install --skip-repo &&
     git -C /workspaces/mictlan remote set-url origin git@mictlan:Xo1otl/mictlan.git && 
     git -C /workspaces/mictlan lfs pull
   ) &
