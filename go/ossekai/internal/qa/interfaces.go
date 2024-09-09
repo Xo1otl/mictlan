4e011e38734cc1380226332d21e7fcc61c12c6a6
Xo1otl <xolotl.mictl4n@gmail.com>
2024-09-09 12:52:20 +0000

    テンソルの勉強した

--- go/ossekai/internal/qa/interfaces.go
+++ go/ossekai/internal/qa/interfaces.go
@@ -4,3 +4,9 @@ type Repo interface {
 	AskQuestion(q Question)
 	Answers(q Question) []Answer
 }
+
+type Storage interface {
+	Store(data []byte) error
+	Retrieve() ([]byte, error)
+	Delete() error
+}
