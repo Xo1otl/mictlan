2c39bf9fccfc7444391b006cf65d883e9b4abd25
Xo1otl <xolotl.mictl4n@gmail.com>
2024-09-10 12:45:29 +0000

    mockdbとmockstorage作成中

--- go/ossekai/internal/qa/interfaces.go
+++ go/ossekai/internal/qa/interfaces.go
@@ -1,12 +1,9 @@
 package qa
 
 type Repo interface {
-	AskQuestion(q Question)
+	AddQuestion(q Question)
 	Answers(q Question) []Answer
 }
 
 type Storage interface {
-	Store(data []byte) error
-	Retrieve() ([]byte, error)
-	Delete() error
 }
