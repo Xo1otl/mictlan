470c24a7909411354adcb5880849d4ffd22d1975
Xo1otl <xolotl.mictl4n@gmail.com>
2024-09-14 06:15:12 +0000

    Fix path issue in postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# fishのpathは適用されないのでフルパスで実行
 (
   (/home/vscode/.bun/bin/bun install --cwd /workspaces/mictlan) &
   (/home/vscode/.local/bin/poetry install --directory /workspaces/mictlan) &
-  (cd /workspaces/mictlan && phpm.sh install) &
+  (cd /workspaces/mictlan && /workspaces/mictlan/scripts/phpm.sh install) &
   (
     git lfs install --skip-repo &&
     git -C /workspaces/mictlan remote set-url origin git@mictlan:Xo1otl/mictlan.git && 
