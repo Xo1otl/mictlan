cc2119c52d8b690636a38cb911a5ad285c5742a5
Xo1otl <xolotl.mictl4n@gmail.com>
2024-09-24 09:27:43 +0000

    minor fix

--- .devcontainer/default/postCreateCommand.sh
+++ .devcontainer/default/postCreateCommand.sh
@@ -12,7 +12,7 @@
   ) &
   (
     git lfs install --skip-repo &&
-    git -C /workspaces/mictlan remote set-url origin git@mictlan:Xo1otl/mictlan.git && 
+    git -C /workspaces/mictlan remote set-url origin git@github:Xo1otl/mictlan.git && 
     git -C /workspaces/mictlan lfs pull &&
     git -C /workspaces/mictlan config pull.rebase true
   ) &
