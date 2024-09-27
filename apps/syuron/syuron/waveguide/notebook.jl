c31e1a1812a9e5e0c8cd514f7e210e75f1876142
Xo1otl <xolotl.mictl4n@gmail.com>
2024-09-27 15:13:06 +0000

    updated vpn config

---
+++ apps/syuron/syuron/waveguide/notebook.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.46
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 341b5f42-1beb-41bc-95b7-1af73791ffc8
+
+
+# ╔═╡ Cell order:
+# ╠═341b5f42-1beb-41bc-95b7-1af73791ffc8
