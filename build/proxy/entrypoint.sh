fc349060dc391f28292d5c7d0203a7063199fc75
Xo1otl <xolotl.mictl4n@gmail.com>
2024-10-02 11:17:07 +0000

    ビルド関連直した

--- build/proxy/entrypoint.sh
+++ build/proxy/entrypoint.sh
@@ -4,7 +4,7 @@
 envsubst '$WG_HOST $WG_WEBUI_PORT' < /etc/nginx/templates/wg-easy.conf.template > /etc/nginx/conf.d/wg-easy.conf
 
 # 動作テストの時は --test-cert をつける
-certbot --nginx --non-interactive --agree-tos -m $CERTBOT_EMAIL -d $WG_HOST
+certbot --nginx --non-interactive --agree-tos -m $CERTBOT_EMAIL -d $WG_HOST --test-cert
 
 # Dockerfileで設定したcronを起動
 crond
