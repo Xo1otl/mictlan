25e109d44c7569f0bf62983c7b6341f628dd6a35
Xo1otl <xolotl.mictl4n@gmail.com>
2024-10-03 08:25:28 +0000

    update docker compose

--- build/vpn/docker-compose.gen.py
+++ build/vpn/docker-compose.gen.py
@@ -1,15 +1,19 @@
+from vpn import *
+
+compose = f"""\
 services:
   wg-easy:
     image: ghcr.io/wg-easy/wg-easy:latest
-    env_file:
-      - .env
     environment:
-      - PASSWORD_HASH=${WG_WEBUI_PASSWORD_HASH} 
-      - PORT=${WG_WEBUI_PORT} # envはグローバルなので一般的すぎる名前はエイリアスにしているが、オーバーライドは同じフォルダの.envの変数のみで可能
+      - WG_HOST={HOST}
+      - PASSWORD_HASH={WEBUI_PASSWORD_HASH} 
+      - WG_PORT={UDP_PORT}
+      - PORT={WEBUI_PORT}
+      - WG_ALLOWED_IPS={ALLOWED_IPS}
       - WG_PERSISTENT_KEEPALIVE=25
       - WG_DEFAULT_DNS= # don't use DNS
     ports:
-      - ${WG_PORT}:${WG_PORT}/udp
+      - {UDP_PORT}:{UDP_PORT}/udp
     cap_add:
       - NET_ADMIN
       - SYS_MODULE
@@ -17,3 +21,9 @@ services:
       - net.ipv4.conf.all.src_valid_mark=1
       - net.ipv4.ip_forward=1
     restart: unless-stopped
+"""
+
+print(compose)
+
+# with open('docker-compose.yaml', 'w') as file:
+#     file.write(compose)
