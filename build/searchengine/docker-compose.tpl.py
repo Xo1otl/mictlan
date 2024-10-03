68220d6d8055e5fdee501554b8838454b5677d22
Xo1otl <xolotl.mictl4n@gmail.com>
2024-10-03 15:37:13 +0000

    renovateがアプデするかためす

--- build/searchengine/docker-compose.tpl.py
+++ build/searchengine/docker-compose.tpl.py
@@ -1,7 +1,7 @@
 compose = f"""\
 services:
   elasticsearch:
-    image: docker.elastic.co/elasticsearch/elasticsearch:8.15.2
+    image: docker.elastic.co/elasticsearch/elasticsearch:8.14.0
     ports:
       - 9200:9200
     environment:
@@ -9,7 +9,7 @@ services:
       - xpack.security.enabled=false
 
   kibana:
-    image: docker.elastic.co/kibana/kibana:8.15.2
+    image: docker.elastic.co/kibana/kibana:8.14.0
     environment:
       ELASTICSEARCH_HOSTS: http://elasticsearch:9200
     ports:
