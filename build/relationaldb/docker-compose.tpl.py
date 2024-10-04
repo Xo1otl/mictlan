7f515f559540231207a1bb70b7fda45ff5618cf8
Xo1otl <xolotl.mictl4n@gmail.com>
2024-10-04 04:24:38 +0000

    yamlをobjectから生成

--- build/relationaldb/docker-compose.tpl.py
+++ build/relationaldb/docker-compose.tpl.py
@@ -1,20 +1,33 @@
+import yaml
 from relationaldb import *
 
-# volumesの部分自動化できそう
-compose = f"""\
-services:
-  mysql:
-    image: mysql:latest
-    ports:
-      - 3306:3306
-    environment:
-      MYSQL_ROOT_PASSWORD: {MYSQL_ROOT_PASSWORD}
-    volumes:
-      - ../koemade/01-initmysqluser.sql:/docker-entrypoint-initdb.d/01-koemade-initmysqluser.sql
-      - ../koemade/02-initmysqltables.sql:/docker-entrypoint-initdb.d/02-koemade-initmysqltables.sql
-      - ../ossekai/01-initmysqluser.sql:/docker-entrypoint-initdb.d/01-ossekai-initmysqluser.sql
-      - ../ossekai/02-initmysqltables.sql:/docker-entrypoint-initdb.d/02-ossekai-initmysqltables.sql
-"""
+# List of SQL scripts to map
+volume_mappings = [
+    ('../koemade/01-initmysqluser.sql',
+     '/docker-entrypoint-initdb.d/01-koemade-initmysqluser.sql'),
+    ('../koemade/02-initmysqltables.sql',
+     '/docker-entrypoint-initdb.d/02-koemade-initmysqltables.sql'),
+    ('../ossekai/01-initmysqluser.sql',
+     '/docker-entrypoint-initdb.d/01-ossekai-initmysqluser.sql'),
+    ('../ossekai/02-initmysqltables.sql',
+     '/docker-entrypoint-initdb.d/02-ossekai-initmysqltables.sql')
+]
 
+# Build the config object
+config = {
+    'services': {
+        'mysql': {
+            'image': 'mysql:latest',
+            'ports': ['3306:3306'],
+            'environment': {
+                'MYSQL_ROOT_PASSWORD': MYSQL_ROOT_PASSWORD
+            },
+            # Automate volume creation
+            'volumes': [f'{src}:{dst}' for src, dst in volume_mappings]
+        }
+    }
+}
+
+# Write the configuration to a docker-compose.yaml file
 with open('docker-compose.yaml', 'w') as file:
-    file.write(compose)
+    yaml.dump(config, file, default_flow_style=False)
--- build/vpn/docker-compose.tpl.py
+++ build/vpn/docker-compose.tpl.py
@@ -1,27 +1,35 @@
+import yaml
 from vpn import *
 
-compose = f"""\
-services:
-  wg-easy:
-    image: ghcr.io/wg-easy/wg-easy:latest
-    environment:
-      - WG_HOST={HOST}
-      - PASSWORD_HASH={WEBUI_PASSWORD_HASH} 
-      - WG_PORT={UDP_PORT}
-      - PORT={WEBUI_PORT}
-      - WG_ALLOWED_IPS={ALLOWED_IPS}
-      - WG_PERSISTENT_KEEPALIVE=25
-      - WG_DEFAULT_DNS= # don't use DNS
-    ports:
-      - {UDP_PORT}:{UDP_PORT}/udp
-    cap_add:
-      - NET_ADMIN
-      - SYS_MODULE
-    sysctls:
-      - net.ipv4.conf.all.src_valid_mark=1
-      - net.ipv4.ip_forward=1
-    restart: unless-stopped
-"""
+config = {
+    'services': {
+        'wg-easy': {
+            'image': 'ghcr.io/wg-easy/wg-easy:latest',
+            'environment': {
+                'WG_HOST': HOST,
+                'PASSWORD_HASH': WEBUI_PASSWORD_HASH,
+                'WG_PORT': UDP_PORT,
+                'PORT': WEBUI_PORT,
+                'WG_ALLOWED_IPS': ALLOWED_IPS,
+                'WG_PERSISTENT_KEEPALIVE': 25,
+                'WG_DEFAULT_DNS': ''  # 空でDNSを使用しない
+            },
+            'ports': [
+                f'{UDP_PORT}:{UDP_PORT}/udp'  # 既存の変数を使用
+            ],
+            'cap_add': [
+                'NET_ADMIN',
+                'SYS_MODULE'
+            ],
+            'sysctls': [
+                'net.ipv4.conf.all.src_valid_mark=1',
+                'net.ipv4.ip_forward=1'
+            ],
+            'restart': 'unless-stopped'
+        }
+    }
+}
 
+# YAMLファイルに出力します
 with open('docker-compose.yaml', 'w') as file:
-    file.write(compose)
+    yaml.dump(config, file, default_flow_style=False)
