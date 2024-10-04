27ba8467dd88edf1e43949b311bf4216b0ced826
Xo1otl <xolotl.mictl4n@gmail.com>
2024-10-04 04:24:38 +0000

    yamlをobjectから生成

--- build/proxy/docker-compose.tpl.py
+++ build/proxy/docker-compose.tpl.py
@@ -1,23 +1,32 @@
+import yaml
 import vpn
 from proxy import *
 
-compose = f"""\
-services:
-  nginx:
-    build:
-      dockerfile: Dockerfile
-    environment:
-      - VPN_HOST={vpn.HOST}
-      - CERTBOT_EMAIL={CERTBOT_EMAIL}
-    volumes:
-      - ./entrypoint.sh:/entrypoint.sh
-      - ../vpn/nginx.conf:/etc/nginx/conf.d/vpn.conf
-    entrypoint: [ "/entrypoint.sh" ]
-    ports:
-      - "80:80/tcp"
-      - "443:443/tcp"
-    restart: unless-stopped
-"""
+# Configuration as a Python object
+config = {
+    'services': {
+        'nginx': {
+            'build': {
+                'dockerfile': 'Dockerfile'
+            },
+            'environment': [
+                f'VPN_HOST={vpn.HOST}',
+                f'CERTBOT_EMAIL={CERTBOT_EMAIL}'
+            ],
+            'volumes': [
+                './entrypoint.sh:/entrypoint.sh',
+                '../vpn/nginx.conf:/etc/nginx/conf.d/vpn.conf'
+            ],
+            'entrypoint': ["/entrypoint.sh"],
+            'ports': [
+                '80:80/tcp',
+                '443:443/tcp'
+            ],
+            'restart': 'unless-stopped'
+        }
+    }
+}
 
+# Write the configuration to a docker-compose.yaml file
 with open('docker-compose.yaml', 'w') as file:
-    file.write(compose)
+    yaml.dump(config, file, default_flow_style=False)
--- build/searchengine/docker-compose.tpl.py
+++ build/searchengine/docker-compose.tpl.py
@@ -1,20 +1,24 @@
-compose = f"""\
-services:
-  elasticsearch:
-    image: docker.elastic.co/elasticsearch/elasticsearch:8.15.2
-    ports:
-      - 9200:9200
-    environment:
-      - discovery.type=single-node
-      - xpack.security.enabled=false
+import yaml
 
-  kibana:
-    image: docker.elastic.co/kibana/kibana:8.15.2
-    environment:
-      ELASTICSEARCH_HOSTS: http://elasticsearch:9200
-    ports:
-      - 5601:5601
-"""
+config = {
+    'services': {
+        'elasticsearch': {
+            'image': 'docker.elastic.co/elasticsearch/elasticsearch:8.14.0',
+            'ports': ['9200:9200'],
+            'environment': [
+                'discovery.type=single-node',
+                'xpack.security.enabled=false'
+            ]
+        },
+        'kibana': {
+            'image': 'docker.elastic.co/kibana/kibana:8.14.0',
+            'environment': {
+                'ELASTICSEARCH_HOSTS': 'http://elasticsearch:9200'
+            },
+            'ports': ['5601:5601']
+        }
+    }
+}
 
 with open('docker-compose.yaml', 'w') as file:
-    file.write(compose)
+    yaml.dump(config, file, default_flow_style=False)
