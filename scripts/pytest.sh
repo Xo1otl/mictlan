cc67d14919b541cc9e6d23d486877ef031e380c7
Xo1otl <xolotl.mictl4n@gmail.com>
2024-10-08 18:25:46 +0000

    微修正

--- scripts/pytest.sh
+++ scripts/pytest.sh
@@ -4,13 +4,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # 共通スクリプトを読み込み
-source "$DIR/buildcommon.sh"
-
-# プロジェクトルートを見つける
-find_project_root
-
-# PYTHONPATHを設定
-set_pythonpath
+source "$DIR/pybuild.sh"
 
 # *.test.py ファイルを実行
 execute_python_files "*.test.py"
