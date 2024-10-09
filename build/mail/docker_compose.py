9eae1ac9f672fa2bf197e52dedf99b55374f0f44
Xo1otl <xolotl.mictl4n@gmail.com>
2024-10-09 19:03:21 +0000

    added mailserver

--- build/mail/docker_compose.py
+++ build/mail/docker_compose.py
@@ -1,3 +1,36 @@
+import ecosystem
+
 docker_compose = {
-    'services': {}
+    "services": {
+        "mailserver": {
+            "image": "ghcr.io/docker-mailserver/docker-mailserver:latest",
+            "container_name": "mailserver",
+            "hostname": f"mail.{ecosystem.DOMAIN}",
+            "domainname"
+            "ports": [
+                # SMTP (explicit TLS => STARTTLS, Authentication is DISABLED => use port 465/587 instead)
+                "25:25",
+                "143:143",  # IMAP4 (explicit TLS => STARTTLS)
+                "465:465",  # ESMTP (implicit TLS)
+                "587:587",  # ESMTP (explicit TLS => STARTTLS)
+                "993:993",  # IMAP4 (implicit TLS)
+            ],
+            "volumes": [
+                "./docker-data/dms/mail-data/:/var/mail/",
+                "./docker-data/dms/mail-state/:/var/mail-state/",
+                "./docker-data/dms/mail-logs/:/var/log/mail/",
+                "./docker-data/dms/config/:/tmp/docker-mailserver/",
+                "/etc/localtime:/etc/localtime:ro"
+            ],
+            "environment": [
+                "ENABLE_RSPAMD=1",
+                "ENABLE_CLAMAV=1",
+                "ENABLE_FAIL2BAN=1",
+            ],
+            "cap_add": [
+                "NET_ADMIN"
+            ],
+            "restart": "always"
+        }
+    }
 }
