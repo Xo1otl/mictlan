beb175ac2b747f9e83c6e33d1873cf6ec8b7467e
Xo1otl <xolotl.mictl4n@gmail.com>
2024-12-20 17:38:56 +0000

    微修正

---
+++ apps/blog/blog/ui/pages/テンソルについて.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+"""
+# テンソルについて
+
+量子力学と機械学習と相対性理論と[アキネーター](https://akinator.mictlan.site)で使われてる最強の数学らしい！？
+"""
---
+++ apps/blog/blog/ui/pages/相対性理論.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+"""
+# 相対性理論
+
+manimで動画つくってstreamlitで表示する方法しりてぇ
+"""
