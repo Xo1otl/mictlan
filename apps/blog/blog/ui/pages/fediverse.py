47b899f733a95cf644bf79f5ed26da7de9349ad9
Xo1otl <xolotl.mictl4n@gmail.com>
2025-01-18 03:09:51 +0000

    いろいろ

---
+++ apps/blog/blog/ui/pages/fediverse.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+"""
+# Fediverse
+
+ActivityHubプロトコルなるものがあって、分散SNSの基本らしい
+
+有名なmastodonやmisskey、pleromaなどはこの実装らしい
+
+実はmisskeyとかわりと古くて、最近海外ではやっててとても新しいlemmyというrust製fediverseを見つけた
+
+[Mictlan](https://lemmy.mictlan.site)で登録してみてください
+"""
