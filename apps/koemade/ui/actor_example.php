8524a1dd5fe7d3289d78f275fc6cdd92464f2671
Xo1otl <xolotl.mictl4n@gmail.com>
2025-01-23 05:37:36 +0000

    anythingllm立てた

--- apps/koemade/ui/actor_example.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../koemade/kernel/bootstrap.php';
-echo "Hello, actor!";
-
-# TODO: 声優が使用する機能をすべて一つのページにまとめたものを作成する、ログイン、プロフィール編集(jsonで直でいじればフォームいらん)、音声の投稿、音声の編集、タグ付けなど(これらもexampleではjsonで直でいじればおｋ)
