7033c3c6f08bc259d7bcc584a37941efe8a34dfe
Xo1otl <xolotl.mictl4n@gmail.com>
2025-02-19 09:26:41 +0000

    blog2追加した。katexかけてmdxでまじ便利

---
+++ apps/blog2/app/routes/_renderer.tsx
@@ -0,0 +1,21 @@
+import { Style } from "hono/css";
+import { jsxRenderer } from "hono/jsx-renderer";
+import { Script } from "honox/server";
+import { Link } from "honox/server";
+
+export default jsxRenderer(({ children, title }) => {
+	return (
+		<html lang="en">
+			<head>
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<Link href="/app/style.css" rel="stylesheet" />
+				<title>{title}</title>
+				<link rel="icon" href="/favicon.ico" />
+				<Script src="/app/client.ts" async />
+				<Style />
+			</head>
+			<body class="prose">{children}</body>
+		</html>
+	);
+});
