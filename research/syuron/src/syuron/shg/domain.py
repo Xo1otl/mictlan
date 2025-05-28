e8bdf7a573a072021192f45a685a6266ea1996d1
Xo1otl <xolotl.mictl4n@gmail.com>
2025-05-28 08:13:24 +0000

    いろいろ

--- research/syuron/src/syuron/shg/domain.py
+++ research/syuron/src/syuron/shg/domain.py
@@ -9,7 +9,7 @@ class Domain(NamedTuple):
 
 type Grating = List[Domain]
 
-# (a, b, 2) の形状を持つテンソル、aはグレーティングの数、bはドメインの数
+# (a, b, 2) の形状を持つテンソル、aはグレーティングの数、bはドメインの数, 最後の2はそれぞれ幅とkappaのこと
 type DomainTensor = jnp.ndarray
 
 # DomainTensor の場合 b がそろってるが、それ以外の型では揃える必要がない
