e9426987fdbb2734bec7f12b0b2f1d26bfd6d04e
Xo1otl <xolotl.mictl4n@gmail.com>
2025-09-05 19:18:33 +0000

    不要なファイルを消した。設計を練っている

--- apps/personal-site/internal/server/README.md
+++
@@ -1,5 +0,0 @@
-# elysia
-
-インフラとして elysia を使った controller layer
-
-どこにも属さない infra layer は普通に internal 直下に作る
--- playground/inspect_fn.py
+++
@@ -1,9 +0,0 @@
-import inspect
-
-def sample_function():
-    # これはコメントです
-    print("Hello, World!")
-
-source = inspect.getsource(sample_function)
-print(source)
-
