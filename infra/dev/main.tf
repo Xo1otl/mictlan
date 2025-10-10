51312c7c6043b4c6ffcffe9e6ab56f9aaee7ceff
Xo1otl <xolotl.mictl4n@gmail.com>
2025-10-10 11:39:21 +0000

    cfの設定をterraformした

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5"
+      version = ">= 5.11.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.3"
     }
   }
 }
@@ -10,16 +14,61 @@ terraform {
 provider "cloudflare" {
 }
 
-resource "cloudflare_zero_trust_access_policy" "xolotl_zero_trust_access_policy" {
-  account_id       = "f92413c21a2212429577cfbede5172dc"
-  decision         = "allow"
-  name             = "xolotl"
-  session_duration = "730h"
-  exclude          = []
+resource "cloudflare_zero_trust_access_policy" "allow_emails" {
+  account_id = var.cloudflare_account_id
+  decision   = "allow"
+  name       = "Allow email addresses"
   include = [{
     email = {
-      email = "xolotl.mictl4n@gmail.com"
+      email = var.cloudflare_email
     }
   }]
-  require = []
+}
+
+resource "cloudflare_zero_trust_device_default_profile" "default_profile" {
+  account_id = var.cloudflare_account_id
+  include = [{
+    address     = "100.96.0.0/12"
+    description = "Include domains in the tunnel"
+  }]
+  tunnel_protocol = "wireguard"
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "mictlan_tunnel" {
+  account_id = var.cloudflare_account_id
+  name       = "mictlan-devcontainer"
+  config_src = "cloudflare"
+}
+
+resource "cloudflare_dns_record" "portfolio" {
+  zone_id = var.cloudflare_zone_id
+  name    = "portfolio"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.mictlan_tunnel.id}.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+data "cloudflare_zero_trust_tunnel_cloudflared_token" "mictlan_tunnel_token" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.mictlan_tunnel.id
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "mictlan_tunnel_config" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.mictlan_tunnel.id
+
+  config = {
+    ingress = [{
+      hostname = "portfolio.mictlan.cc"
+      service  = "http://devcontainer:1111"
+      }, {
+      service = "http_status:404"
+    }]
+  }
+}
+
+resource "local_file" "tunnel_token_file" {
+  content  = data.cloudflare_zero_trust_tunnel_cloudflared_token.mictlan_tunnel_token.token
+  filename = "${path.module}/cloudflared-token"
 }
---
+++ infra/dev/variables.tf
@@ -0,0 +1,3 @@
+variable "cloudflare_account_id" {}
+variable "cloudflare_email" {}
+variable "cloudflare_zone_id" {}
